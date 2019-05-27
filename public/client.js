25550690c40e87f62e1811c226a228ff3f724773
thembones79 <michal.szumnarski@gmail.com>
2019-05-27 22:26:09 +0200

    done

--- public/client.js
+++ public/client.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
-  // Form submittion with new message in field with id 'm'
+  /*global io*/
+  var socket = io();
+
   $("form").submit(function() {
     var messageToSend = $("#m").val();
-    //send message to server here?
+    socket.emit("chat message", messageToSend);
     $("#m").val("");
     return false; // prevent form submit from refreshing page
   });
 
-  /*global io*/
-  var socket = io();
-  socket.on("user count", function(data) {
-    console.log(data);
+  socket.on("user", function(data) {
+    $("#num-users").text(data.currentUsers + " users online");
+    var message = data.name;
+    if (data.connected) {
+      message += " has joined the chat.";
+    } else {
+      message += " has left the chat.";
+    }
+    $("#messages").append($("<li>").html("<b>" + message + "</b>"));
+  });
+
+  socket.on("chat message", function(data) {
+    $("#messages").append($("<li>").text(data.name + ": " + data.message));
   });
 });
--- server.js
+++ server.js
@@ -7,15 +7,13 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const auth = require("./app/auth.js");
 const routes = require("./app/routes.js");
 const mongo = require("mongodb").MongoClient;
-const passport = require("passport");
 const cookieParser = require("cookie-parser");
-
+const passport = require("passport");
+const sessionStore = new session.MemoryStore();
 const app = express();
 const http = require("http").Server(app);
-const sessionStore = new session.MemoryStore();
 const io = require("socket.io")(http);
-const cors = require("cors");
-app.use(cors());
+const passportSocketIo = require("passport.socketio");
 
 fccTesting(app); //For FCC testing purposes
 
@@ -45,14 +43,37 @@ mongo.connect(process.env.DATABASE, (err, db) => {
 
   //start socket.io code
 
+  io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: "express.sid",
+      secret: process.env.SESSION_SECRET,
+      store: sessionStore
+    })
+  );
+
   var currentUsers = 0;
+
   io.on("connection", socket => {
     ++currentUsers;
-    console.log("A user has connected");
-    io.emit("user count", currentUsers);
-    console.log("currentUsers: " + currentUsers);
+    console.log("user " + socket.request.user.name + " connected");
+    io.emit("user", {
+      name: socket.request.user.name,
+      currentUsers,
+      connected: true
+    });
+
+    socket.on("chat message", message => {
+      io.emit("chat message", { name: socket.request.user.name, message });
+    });
+
     socket.on("disconnect", () => {
       --currentUsers;
+      io.emit("user", {
+        name: socket.request.user.name,
+        currentUsers,
+        connected: true
+      });
     });
   });
 
